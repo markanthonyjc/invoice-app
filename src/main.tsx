b305e54b9c5241252d69ea021d7c86bb3219ca93
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-10-31 17:36:17 -0500

    Refactoring code and install vitest package

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+root.render(<App/>)
