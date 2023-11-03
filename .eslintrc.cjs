2fc99780ca7db239f4734fde8ae60a5d55b5c343
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-11-03 09:58:36 -0500

    Add triple slash reference in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,18 @@
 module.exports = {
+  env: {
+    browser: true, es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [{
     env: {
-        browser: true, es2021: true
-    }, extends: ["standard-with-typescript", "plugin:react/recommended"], overrides: [{
-        env: {
-            node: true
-        }, files: [".eslintrc.{js,cjs}"], parserOptions: {
-            sourceType: "script"
-        }
-    }], parserOptions: {
-        ecmaVersion: "latest", sourceType: "module", project: "./tsconfig.json"
-    }, plugins: ["react"], rules: {'react/react-in-jsx-scope': 'off', '@typescript-eslint/explicit-function-return-type': 'off'}
+      node: true
+    }, files: ['.eslintrc.{js,cjs}'], parserOptions: {
+      sourceType: 'script'
+    }
+  }],
+  parserOptions: {
+    ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: { 'react/react-in-jsx-scope': 'off', '@typescript-eslint/explicit-function-return-type': 'off', '@typescript-eslint/triple-slash-reference': 'off' }
 }
