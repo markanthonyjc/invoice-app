a3e4175c583abcf04df817ce600e6782f003acf7
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-11-05 13:13:09 -0500

    Add resource files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,14 @@ import IconMoon from './icon-moon.svg'
 import Logo from './logo.svg'
 import LogoBackground from './logo-background-mobile.svg'
 import ImageAvatar from './image-avatar.jpg'
+import IconArrowDown from './icon-arrow-down.svg'
+import IconPlus from './icon-plus.svg'
 
 export {
   IconMoon,
   Logo,
   LogoBackground,
-  ImageAvatar
+  ImageAvatar,
+  IconArrowDown,
+  IconPlus
 }
