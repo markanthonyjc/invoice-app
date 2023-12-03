d86ae8c17536ec8f95cbed88ffc107462a9d643e
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-12-02 20:24:28 -0500

    Add invoice style

--- src/pages/invoice/Invoice.tsx
+++ src/pages/invoice/Invoice.tsx
@@ -1,9 +1,10 @@
 import InvoiceHeader from './invoice-header/InvoiceHeader'
 import InvoiceList from './invoice-list/InvoiceList'
+import style from './Invoice.module.scss'
 
 const Invoice = () => {
   return (
-        <section>
+        <section className={style.invoice}>
             <InvoiceHeader />
             <InvoiceList />
         </section>
